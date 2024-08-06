d9875ae67a6419897a8b464b55701a12a0fcb14a
arjun-som11 <arjun.singh@lumiq.ai>
2024-08-06 13:16:13 +0530

    Resolved Conflicts

--- Fifth/src/main/java/org/example/controller/ExcelController.java
+++ Fifth/src/main/java/org/example/controller/ExcelController.java
@@ -1,9 +1,11 @@
 package org.example.controller;
 
 import org.example.service.ExcelService;
+import org.example.service.ParkingDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +13,20 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class ExcelController {
     @Autowired
     ExcelService excelService;
+    @Autowired
+    ParkingDataService parkingDataService;
 
 
     @PostMapping("/upload")
     public ResponseEntity<Map<String, String>> uploadExcelFile(@RequestParam("file") MultipartFile file) {
-        Map<String, String> response = null;
         try {
-            response = excelService.kimportExcelData(file);
+            Map<String, String> response = excelService.kimportExcelData(file);
             // Return ResponseEntity based on the response map's status
             if ("200".equals(response.get("statusCode"))) {
                 return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -31,7 +35,21 @@ public class ExcelController {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
             }
         } catch (IOException e) {
+            Map<String, String> response = Map.of("statusMessage", "Failed to process file: " + e.getMessage(), "statusCode", "500");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
+    }
+
+    @GetMapping("/getParkingData")
+    public ResponseEntity<Map<String, Object>> getParkingData(@RequestParam("applicatinNo") String applicationNo) {
+
+        Map<String, Object> response = parkingDataService.getParkingData(applicationNo);
+        if (response.containsValue("200")) {
+            return ResponseEntity.status(HttpStatus.FOUND).body(response);
+        } else {
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+
     }
 }
--- Fifth/src/main/java/org/example/repository/PayBankParkingRepository.java
+++ Fifth/src/main/java/org/example/repository/PayBankParkingRepository.java
@@ -2,8 +2,14 @@ package org.example.repository;
 
 import org.example.model.PayBankParkingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PayBankParkingRepository extends JpaRepository<PayBankParkingEntity,Long> {
+
+    @Query("select p from PayBankParkingEntity p where p.application_no =?1")
+    PayBankParkingEntity  getDataByApplicationNo(String applicationNo);
+
+
 }
