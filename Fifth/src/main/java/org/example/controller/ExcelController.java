7a077db943d3537185b9b36534604635ff47cf24
arjun-som11 <arjun.singh@lumiq.ai>
2024-08-06 12:20:12 +0530

    EXL || adding  new changes related to new getoparkingdata ap PARKING SERVICE || Adding parking service class

--- Fifth/src/main/java/org/example/controller/ExcelController.java
+++ Fifth/src/main/java/org/example/controller/ExcelController.java
@@ -1,22 +1,26 @@
 package org.example.controller;
 
 import org.example.service.ExcelService;
+import org.example.service.ParkingDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class ExcelController {
     @Autowired
     ExcelService excelService;
+    @Autowired
+    ParkingDataService parkingDataService;
 
 
     @PostMapping("/upload")
@@ -31,11 +35,21 @@ public class ExcelController {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
             }
         } catch (IOException e) {
-            Map<String, String> response = Map.of(
-                    "statusMessage", "Failed to process file: " + e.getMessage(),
-                    "statusCode", "500"
-            );
+            Map<String, String> response = Map.of("statusMessage", "Failed to process file: " + e.getMessage(), "statusCode", "500");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
+    }
+
+    @GetMapping("/getParkingData")
+    public ResponseEntity<Map<String, Object>> getParkingData(@RequestParam("applicatinNo") String applicationNo) {
+
+        Map<String, Object> response = parkingDataService.getParkingData(applicationNo);
+        if (response.containsValue("200")) {
+            return ResponseEntity.status(HttpStatus.FOUND).body(response);
+        } else {
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+
     }
 }
