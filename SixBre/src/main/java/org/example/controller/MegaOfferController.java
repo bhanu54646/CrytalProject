d5812839dade2340ab8250e75e5c08775beae22f
arjun-som11 <arjun.singh@lumiq.ai>
2024-08-23 12:02:28 +0530

    Modifying request and reponse by using Gson and ObjectMapper class

--- SixBre/src/main/java/org/example/controller/MegaOfferController.java
+++ SixBre/src/main/java/org/example/controller/MegaOfferController.java
@@ -1,5 +1,8 @@
 package org.example.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import org.example.dao.ClaimInvestigationRequest;
 import org.example.dao.ClaimInvestigationResponse;
 import org.example.dao.DiscountResponse;
@@ -22,21 +25,30 @@ public class MegaOfferController {
     @Autowired
     RequestLoggingRepo requestLoggingRepo;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final Gson gson = new Gson(); // Gson instance
+
     @PostMapping("/getdiscountpercent")
     public DiscountResponse getdiscount(@RequestBody Order request) throws IOException {
         return discount.getdiscount(request);
     }
 
     @PostMapping("/execute/investigation/rule/v1")
-    public ClaimInvestigationResponse executeInvestigationRule(@RequestBody ClaimInvestigationRequest request) {
+    public ClaimInvestigationResponse executeInvestigationRule(@RequestBody ClaimInvestigationRequest request) throws JsonProcessingException {
         RequestLogging requestlog = new RequestLogging();
         ClaimInvestigationResponse response = discount.executeInvestigationRule(request);
         requestlog.setApiType("Investigation-Rules-Api");
         requestlog.setRequestMethod("POST");
         requestlog.setModuleName("BRE");
-        requestlog.setRequestBody("ClaimAmount"+"="+request.getClaimAmount() + " "+"CauseOfDeath"+"="+ request.getCauseOfDeath());
+        // Convert request object to JSON string using Gson
+        String requestBodyJson = gson.toJson(request);
+        requestlog.setRequestBody(requestBodyJson);
+        //  String requestBodyJson = objectMapper.writeValueAsString(request);
+        requestlog.setRequestBody(requestBodyJson);
         requestlog.setRequestEndpoint("EndPoint :/execute/investigation/rule/v1");
-        requestlog.setResponseBody("InvestigationRequired" + "="+response.getInvestigationRequired() +" "+ "statusMessage"+ "="+ "" + response.getStatusMessage());
+        String responseBodyJson = gson.toJson(response);
+        // String responseBodyJson = objectMapper.writeValueAsString(response);
+        requestlog.setResponseBody(responseBodyJson);
         requestLoggingRepo.save(requestlog);
         return response;
     }
