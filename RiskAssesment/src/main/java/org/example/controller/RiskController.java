24b514ac847e5f46adb3da1231f8263c760b1dc7
arjun-som11 <arjun.singh@lumiq.ai>
2024-12-09 12:00:39 +0530

    using requestLOgofBRE Service in othermodules

--- RiskAssesment/src/main/java/org/example/controller/RiskController.java
+++ RiskAssesment/src/main/java/org/example/controller/RiskController.java
@@ -1,9 +1,11 @@
 package org.example.controller;
-
-
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.example.dao.RiskCalculateRequest;
 import org.example.dao.RiskResponse;
+import org.example.model.RequestLogging;
+import org.example.model.RiskProfile;
+import org.example.repository.RequestLoggingRepo;
 import org.example.service.RiskServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class RiskController {
    @Autowired
     RiskServiceImpl  riskService;
+    @Autowired
+    RequestLoggingRepo requestLoggingRepo;
+
+    private final Gson gson = new Gson();
 
     private static final Logger log = LoggerFactory.getLogger(RiskController.class);
 
@@ -26,9 +32,21 @@ public class RiskController {
     {
         log.info("###################----->>>>inside riskController");
         log.info("###################----->>>>request  for getRiskratingis"+request);
-
-        return riskService.calculateRisk(request);
-
+        RiskResponse res=riskService.calculateRisk(request);
+        log.info("###################----->>>Response is"+res);
+        String requestBodyJson = gson.toJson(request);
+        String responseBodyJson=gson.toJson(res);
+        RequestLogging r=new RequestLogging();
+        if(res!=null)
+        {
+           r.setModuleName("Risk-MODULE");
+           r.setApiType("/get/rating");
+           r.setRequestMethod("POST");
+           r.setRequestBody(requestBodyJson);
+           r.setResponseBody(responseBodyJson);
+           requestLoggingRepo.save(r);
+        }
+        return res;
     }
 }
 
