be854cbac234960d2f91bebda61dd8077243d6b8
arjun-som11 <arjun.singh@lumiq.ai>
2024-12-18 18:11:17 +0530

    adding api totrigger riskapi

--- RiskAssesment/src/main/java/org/example/controller/RiskController.java
+++ RiskAssesment/src/main/java/org/example/controller/RiskController.java
@@ -1,4 +1,5 @@
 package org.example.controller;
+
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.example.dao.RiskCalculateRequest;
@@ -10,16 +11,14 @@ import org.example.service.RiskServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @Slf4j
 @RestController
 @RequestMapping("/risk")
 public class RiskController {
-   @Autowired
-    RiskServiceImpl  riskService;
+    @Autowired
+    RiskServiceImpl riskService;
     @Autowired
     RequestLoggingRepo requestLoggingRepo;
 
@@ -28,24 +27,28 @@ public class RiskController {
     private static final Logger log = LoggerFactory.getLogger(RiskController.class);
 
     @PostMapping("/get/rating")
-    public RiskResponse calculateRisk(@RequestBody RiskCalculateRequest request)
-    {
+    public RiskResponse calculateRisk(@RequestBody RiskCalculateRequest request) {
         log.info("###################----->>>>inside riskController");
-        log.info("###################----->>>>request  for getRiskratingis"+request);
-        RiskResponse res=riskService.calculateRisk(request);
-        log.info("###################----->>>Response is"+res);
+        log.info("###################----->>>>request  for getRiskratingis" + request);
+        RiskResponse res = riskService.calculateRisk(request);
+        log.info("###################----->>>Response is" + res);
         String requestBodyJson = gson.toJson(request);
-        String responseBodyJson=gson.toJson(res);
-        RequestLogging r=new RequestLogging();
-        if(res!=null)
-        {
-           r.setModuleName("Risk-MODULE");
-           r.setApiType("/get/rating");
-           r.setRequestMethod("POST");
-           r.setRequestBody(requestBodyJson);
-           r.setResponseBody(responseBodyJson);
-           requestLoggingRepo.save(r);
+        String responseBodyJson = gson.toJson(res);
+        RequestLogging r = new RequestLogging();
+        if (res != null) {
+            r.setModuleName("Risk-MODULE");
+            r.setApiType("/get/rating");
+            r.setRequestMethod("POST");
+            r.setRequestBody(requestBodyJson);
+            r.setResponseBody(responseBodyJson);
+            requestLoggingRepo.save(r);
         }
+        return res;
+    }
+
+    @PostMapping("/trigger/v1")
+    public RiskResponse triggerriskapi(@RequestBody RiskCalculateRequest request) {
+        RiskResponse res = riskService.calculateRisk(request);
         return res;
     }
 }
